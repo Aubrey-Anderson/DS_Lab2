e4c5844f2468af89452c1ee9af46173119dbe6bb
Aubrey Anderson <aubrey.anderson@centre.edu>
2026-09-13 15:17:04 -0400

    Implemented and corrected testWeeklyEventDisplacement. Worked on simplifying and cleaning the tests

--- src/test/java/CalendarEventTest.java
+++ src/test/java/CalendarEventTest.java
@@ -7,146 +7,133 @@ import calendar.MeetingCalendar;
 
 class CalendarEventTest
 {
-	Meeting A;
-	Meeting B;
-	Meeting C;
-	
+    MeetingCalendar cal;
 
-	Meeting AB;
-	Meeting BC;
-	
-	GregorianCalendar startA;
-	GregorianCalendar endA;
-	GregorianCalendar startAB;
-	GregorianCalendar endAB;
-	GregorianCalendar endB;
-	GregorianCalendar endC;
-	GregorianCalendar repeatUntil;
-	
-	MeetingCalendar cal;
-	
-	OneTimeEvent eventA;
-	OneTimeEvent eventB;
-	
+    GregorianCalendar startA;
+    GregorianCalendar endA;
 
+    GregorianCalendar startB;
+    GregorianCalendar endB;
+
+    GregorianCalendar startC;
+    GregorianCalendar endC;
+
+    GregorianCalendar repeatUntil;
+
+
+    @BeforeEach
+    void setUp() throws Exception
+    {
+        cal = new MeetingCalendar();
+
+        startA = new GregorianCalendar(2023, 8, 28, 8, 30);
+        endA = new GregorianCalendar(2023, 8, 28, 9, 30);
+
+        startB = new GregorianCalendar(2023, 8, 28, 9, 30);
+        endB = new GregorianCalendar(2023, 8, 28, 10, 30);
+
+        startC = new GregorianCalendar(2023, 8, 28, 10, 30);
+        endC = new GregorianCalendar(2023, 8, 28, 11, 30);
+    }
+
+    @Test
+    void testOneTimeEvent()
+    {
+    	GregorianCalendar emptyTime = new GregorianCalendar(2023, 8, 28, 10, 00);
+        OneTimeEvent eventA = new OneTimeEvent("Meeting A", "Location A", startA, endA);
+        OneTimeEvent duplicate = new OneTimeEvent("Meeting Dupe", "Location A", startA, endA);
+
+        assertEquals("Meeting A", eventA.getDescription());
+        assertEquals("Location A", eventA.getLocation());
+        assertEquals(startA, eventA.getStartTime());
+        assertEquals(endA, eventA.getEndTime());
+
+
+        assertNull(cal.findMeeting(startA));
+        eventA.scheduleEvent(cal);
+        duplicate.scheduleEvent(cal);
+        Meeting meeting = cal.findMeeting(startA);
+        assertNotNull(meeting);
+        assertEquals("Meeting A", meeting.getDescription());
+
+        assertNull(cal.findMeeting(emptyTime));
+        
+    }
+
+    @Test
+    void testPriorityEvent()
+    {
+    	GregorianCalendar endingB = new GregorianCalendar(2023, 8, 28, 12, 30);
+        OneTimeEvent eventA = new OneTimeEvent("Meeting A", "Location A", startA, endA);
+        OneTimeEvent eventB = new OneTimeEvent("Meeting B", "Location B", startB, endingB);
+        PriorityEvent eventC = new PriorityEvent("Meeting C", "Location C", startB, endC);
+
+        eventA.scheduleEvent(cal);
+        assertNotNull(cal.findMeeting(startA));
+        assertEquals("Meeting A", cal.findMeeting(startA).getDescription());
+        
+        eventB.scheduleEvent(cal);
+        assertNotNull(cal.findMeeting(startB));
+        assertEquals("Meeting B", cal.findMeeting(startB).getDescription());
+
+        eventC.scheduleEvent(cal);
+        assertNotNull(cal.findMeeting(startB));
+        assertEquals("Meeting C", cal.findMeeting(startB).getDescription());
+
+        assertNotNull(cal.findMeeting(startA));
+        assertNull(cal.findMeeting(endingB));
+        
+    }
 	
-	@BeforeEach
-	void setUp() throws Exception
-	{
-		
-		cal = new MeetingCalendar();
-		startA = new GregorianCalendar(2023,8,28,8,30);
-		endA = new GregorianCalendar(2023,8,28,9,30);
-		endB = new GregorianCalendar(2023,8,28,10,30);
-		endC = new GregorianCalendar(2023,8,28,11,30);
-		
-		startAB = new GregorianCalendar(2023,8,28,9,00);
-		endAB = new GregorianCalendar(2023,8,28,10,00);
-		
-		repeatUntil = new GregorianCalendar(2024,8,28,9,00);
-
-		
-		
-	}
-
-	@Test
-	void testOneTimeEvent()
-	{
-		eventA = new OneTimeEvent("Meeting A", "location A", startA, endA);
-		eventB = new OneTimeEvent("Meeting B", "location B", startAB, endB);
-		
-		assertEquals("Meeting A", eventA.getDescription());
-		assertEquals("location A", eventA.getLocation());
-		assertEquals(startA, eventA.getStartTime());
-		assertEquals(endA, eventA.getEndTime());
-		
-		eventA.setDescription("changed A");
-		assertEquals("changed A", eventA.getDescription());
-		eventA.setLocation("new location");
-		assertEquals("new location", eventA.getLocation());
-		eventA.setStartTime(startAB);
-		assertEquals(startAB, eventA.getStartTime());
-		eventA.setEndTime(endAB);
-		assertEquals(endAB, eventA.getEndTime());
-		
-		eventA.scheduleEvent(cal);
-		eventB.scheduleEvent(cal);
-	    assertEquals(eventA.getDescription(), (cal.findMeeting(startAB)).getDescription());
-		
-	}
-	
-	@Test
-	void testPriorityEvent()
-	{
-		PriorityEvent eventC = new PriorityEvent("Meeting C", "location C", startAB, endA);
-		eventA = new OneTimeEvent("Meeting A", "location A", startA, endA);
-		
-		assertEquals("Meeting C", eventC.getDescription());
-		assertEquals("location C", eventC.getLocation());
-		assertEquals(startAB, eventC.getStartTime());
-		assertEquals(endA, eventC.getEndTime());
-		
-		eventC.setDescription("changed C");
-		assertEquals("changed C", eventC.getDescription());
-		eventC.setLocation("new location");
-		assertEquals("new location", eventC.getLocation());
-		eventC.setStartTime(startA);
-		assertEquals(startA, eventC.getStartTime());
-		eventC.setEndTime(endAB);
-		assertEquals(endAB, eventC.getEndTime());
-		
-
-		eventA.scheduleEvent(cal);
-		eventC.scheduleEvent(cal);
-		assertEquals(eventC.getDescription(), (cal.findMeeting(startA)).getDescription());
-		
-	}
 	
 	@Test 
 	void testWeeklyEvent()
 	{
-		GregorianCalendar startE = new GregorianCalendar(2025,9,6,9,00);
-		GregorianCalendar endE = new GregorianCalendar(2025,9,6,10,00);
-		WeeklyEvent eventD = new WeeklyEvent("Meeting D", "Location D", startA, endA, repeatUntil);
-		WeeklyEvent eventE = new WeeklyEvent("Meeting E", "Location E", startE, endE, repeatUntil);
-		eventB = new OneTimeEvent("Meeting B", "location B", startE, endE);
+		GregorianCalendar weeklyStart = new GregorianCalendar(2023, 8, 28, 8, 30);
+	    GregorianCalendar weeklyEnd = new GregorianCalendar(2023, 8, 28, 9, 30);
+	    GregorianCalendar weeklyRepeatUntil = new GregorianCalendar(2024, 8, 28, 9, 30);
+	    
+	    WeeklyEvent eventW = new WeeklyEvent("Meeting W", "Location W", weeklyStart, weeklyEnd, weeklyRepeatUntil);
+	    assertEquals("Meeting W", eventW.getDescription());
+	    assertEquals("Location W", eventW.getLocation());
+	    assertEquals(weeklyStart, eventW.getStartTime());
+	    assertEquals(weeklyEnd, eventW.getEndTime());
+   //   assertEquals(weeklyRepeatUntil, eventW.getRepeatsUntil());
+	    
+	    GregorianCalendar week2 = new GregorianCalendar(2023, 9, 5, 8, 30);
+	    GregorianCalendar lastWeek = new GregorianCalendar(2024, 8, 26, 8, 30);
+	    GregorianCalendar invalidWeek = new GregorianCalendar(2024, 9, 4, 8, 30);
 		
-		GregorianCalendar newRepeatUntil = new GregorianCalendar(2024,9,4,9,00);
+		eventW.scheduleEvent(cal);
+		assertNotNull(cal.findMeeting(weeklyStart));
+		assertEquals("Meeting W", cal.findMeeting(weeklyStart).getDescription());
 		
-		assertEquals("Meeting D", eventD.getDescription());
-		assertEquals("Location D", eventD.getLocation());
-		assertEquals(startA, eventD.getStartTime());
-		assertEquals(endA, eventD.getEndTime());
-		assertEquals(repeatUntil, eventD.getRepeatsUntil());
+		assertNotNull(cal.findMeeting(week2));
+		assertEquals("Meeting W", cal.findMeeting(week2).getDescription());
 		
-		eventD.setDescription("changed D");
-		assertEquals("changed D", eventD.getDescription());
-		eventD.setLocation("new location");
-		assertEquals("new location", eventD.getLocation());
-		eventD.setStartTime(startAB);
-		assertEquals(startAB, eventD.getStartTime());
-		eventD.setEndTime(endAB);
-		assertEquals(endAB, eventD.getEndTime());
-		eventE.setRepeatsUntil(newRepeatUntil);
-		assertEquals(newRepeatUntil, eventE.getRepeatsUntil());
+		assertNotNull(cal.findMeeting(lastWeek));
+		assertEquals("Meeting W", cal.findMeeting(lastWeek).getDescription());
 		
-		eventD.scheduleEvent(cal);
+		assertNull(cal.findMeeting(invalidWeek));
 		
-		//GregorianCalendar secondMeeting = new GregorianCalendar(2023,9,4,9,00);
-		//GregorianCalendar lastMeeting = new GregorianCalendar(2024,8,28,9,00);
-		//GregorianCalendar nonexistentMeeting = new GregorianCalendar(2024,9,4,9,00);
-		
-		assertEquals(eventD.getDescription(), (cal.findMeeting(startAB)).getDescription());
-	//	assertEquals(eventD.getDescription(), (cal.findMeeting(secondMeeting)).getDescription());
-	//	assertEquals(eventD.getDescription(), (cal.findMeeting(lastMeeting)).getDescription());
-	//	assertNull(cal.findMeeting(nonexistentMeeting));
-		
-		eventB.scheduleEvent(cal);
-		eventE.scheduleEvent(cal);
-		
-		assertEquals(eventB.getDescription(), cal.findMeeting(startE));
 	}
-	
+	@Test
+	void testWeeklyEventDisplacement()
+	{
+		GregorianCalendar weeklyRepeatUntil = new GregorianCalendar(2024, 8, 28, 9, 30);
+		OneTimeEvent eventA = new OneTimeEvent("Meeting A", "Location A", startA, endA);
+		WeeklyEvent overlap = new WeeklyEvent("Meeting Dupe", "Location Duplicate", startA, endA, weeklyRepeatUntil);
+		
+		eventA.scheduleEvent(cal);
+		assertNotNull(cal.findMeeting(startA));
+		overlap.scheduleEvent(cal);
+        assertEquals("Meeting A", cal.findMeeting(startA).getDescription());
+        
+        GregorianCalendar dupeWeek2 = new GregorianCalendar(2023, 9, 5, 8, 30);
+        assertNotNull(cal.findMeeting(dupeWeek2));
+        assertEquals("Meeting Dupe", cal.findMeeting(dupeWeek2).getDescription());
+        
+	}
 
 
 }
