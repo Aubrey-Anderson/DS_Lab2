d2890ef8abf2d7ebb4b112abd986908ca33e21e2
Aubrey Anderson <aubrey.anderson@centre.edu>
2026-09-15 16:21:43 -0400

    Got WeeklyEvent implemented.

--- src/main/java/WeeklyEvent.java
+++ src/main/java/WeeklyEvent.java
@@ -28,19 +28,9 @@ public class WeeklyEvent extends CalendarEvent
 	@Override
 	public void scheduleEvent(MeetingCalendar cal)
 	{
-		GregorianCalendar weekEndTime = getEndTime();
-		GregorianCalendar i = getStartTime();
-		/*for(GregorianCalendar i = getStartTime(); i.compareTo(getRepeatUntil()) > 0; i.add(Calendar.DAY_OF_MONTH, 7))
-		{
-			weekEndTime.add(Calendar.DAY_OF_MONTH, 7);
-			Meeting meeting = new Meeting(getDescription(), getLocation(), i, weekEndTime);
-			if(!cal.doesMeetingConflict(meeting))
-			{
-				cal.addMeeting(meeting);
-			}
-		}
-		*/
-		while(i.before(getRepeatUntil()) || i.equals(getRepeatUntil()))
+		GregorianCalendar weekEndTime = (GregorianCalendar)getEndTime().clone();
+		GregorianCalendar i = (GregorianCalendar)getStartTime().clone();
+		while(i.before(getRepeatUntil()))
 		{
 			Meeting meeting = new Meeting(getDescription(), getLocation(), i, weekEndTime);
 			if(!cal.doesMeetingConflict(meeting))
